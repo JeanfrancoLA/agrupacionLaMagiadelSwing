e228eba2005cc10d2bc4cbd53f761dd033e547ad
JeanfrancoLA <genkoz98@gmail.com>
2022-03-03 14:50:04 -0500

    solo son pruebas

---
+++ js/index.js
@@ -0,0 +1,37 @@
+'use strict';
+const documentReady = () => {
+
+    // Start header
+  const header = document.querySelector('.header');
+  const headerNav = document.querySelector('.header-nav');
+  const headerNavMenuIconContainer = document.getElementById('headerNavMenuIconContainer');
+  const headerNavMenuIcon = document.querySelector('.header-nav__menu-icon');
+  const headerNavCloseIconContainer = document.getElementById('headerNavCloseIconContainer');
+  const headerNavLinkList = document.querySelector('.header-nav__link-list');
+  const headerNavLinks = [...document.querySelectorAll('.header-nav__link')];
+
+  const documentScroll = () => {
+    header.classList.toggle('header--scroll', window.scrollY > 0);
+    headerNav.classList.toggle('header-nav--scroll', window.scrollY > 0);
+    headerNavMenuIcon.classList.toggle('header-nav__menu-icon--scroll', window.scrollY > 0);
+    headerNavLinks.map((element) => {
+      element.classList.toggle('header-nav__link--scroll', window.scrollY > 0);
+    });
+  };
+
+  const openMenu = () => {
+    headerNavLinkList.classList.add('header-nav__link-list--open');
+  };
+
+  const closeMenu = () => {
+    headerNavLinkList.classList.remove('header-nav__link-list--open');
+  };
+
+  document.addEventListener('scroll', documentScroll);
+  headerNavMenuIconContainer.addEventListener('click', openMenu);
+  headerNavCloseIconContainer.addEventListener('click', closeMenu);
+  headerNavLinks.map((element) => {
+    element.addEventListener('click', closeMenu);
+  });
+}  // End header
+document.addEventListener('DOMContentLoaded', documentReady);
